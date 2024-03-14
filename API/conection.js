182693e0b4927a395853b4fbc2cb4a467d9dfe50
Deyvid-10 <marmolejodeyvid@gmail.com>
2024-03-13 22:20:07 -0400

    .

--- API/conection.js
+++ API/conection.js
@@ -1,11 +1,17 @@
 const mysql = require('mysql');
+const dotenv = require('dotenv');
+const path = require('path');
+
+dotenv.config({
+  path: path.resolve(__dirname, process.env.NODE_ENV + '.env')
+});
 
 // Configuración de la conexión a la base de datos
 const conexion = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'Contrasena20',
-  database: 'tauro_shop' 
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE 
 });
 
 // Conectar la base de datos
